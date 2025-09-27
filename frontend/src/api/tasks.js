dec22b9894e0dcad0916cc3cc1c50fef48f32e90
Chelshiya <117815599+Chelshiyaa@users.noreply.github.com>
2025-09-27 17:12:42 +0530

    changes

--- frontend/src/api/tasks.js
+++ frontend/src/api/tasks.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = import.meta.env.VITE_PUBLIC_API_URL; 
+const API_URL = import.meta.env.VITE_PUBLIC_API_URL;
 
 export const getTasks = async (params = {}) => {
   const res = await axios.get(API_URL, { params });
